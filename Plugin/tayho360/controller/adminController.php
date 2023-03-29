ce593ad8f5b0e56a0a6eebe5292678448907a5b9
huubang97 <69882867+huubang97@users.noreply.github.com>
2023-03-29 16:27:22 +0700

    Update adminController.php

--- Plugin/tayho360/controller/adminController.php
+++ Plugin/tayho360/controller/adminController.php
@@ -829,4 +829,169 @@ function deleteCraftvillageAdmin($input){
 
     return $controller->redirect('/plugins/admin/tayho360-admin-craftvillage-listCraftvillageAdmin.php?status=3');
 }
+
+// Nhà hàng Restaurant
+
+function listRestaurantAdmin($input)
+{
+    global $controller;
+    global $urlCurrent;
+    global $modelCategories;
+    global $metaTitleMantan;
+
+    $metaTitleMantan = 'Danh sách Cơ quan hành chính';
+
+    $modelRestaurant = $controller->loadModel('Restaurants');
+    
+    $conditions = array();
+     if(!empty($_GET['name'])){
+        $key=createSlugMantan($_GET['name']);
+
+        $conditions['urlSlug LIKE']= '%'.$key.'%';
+    }
+    $limit = 20;
+    $page = (!empty($_GET['page']))?(int)$_GET['page']:1;
+    if($page<1) $page = 1;
+    $order = array('id'=>'desc');
+    
+    $listData = $modelRestaurant->find()->limit($limit)->page($page)->where($conditions)->order($order)->all()->toList();
+
+    if(!empty($listData)){
+        foreach ($listData as $key => $value) {
+            $conditions_scan = array('id'=>$value->id);
+            $static = $modelRestaurant->find()->where($conditions_scan)->all()->toList();
+            $listData[$key]->number_scan = count($static);
+        }
+    }
+
+    // phân trang
+    $totalData = $modelRestaurant->find()->where($conditions)->all()->toList();
+    $totalData = count($totalData);
+
+    $balance = $totalData % $limit;
+    $totalPage = ($totalData - $balance) / $limit;
+    if ($balance > 0)
+        $totalPage+=1;
+
+    $back = $page - 1;
+    $next = $page + 1;
+    if ($back <= 0)
+        $back = 1;
+    if ($next >= $totalPage)
+        $next = $totalPage;
+
+    if (isset($_GET['page'])) {
+        $urlPage = str_replace('&page=' . $_GET['page'], '', $urlCurrent);
+        $urlPage = str_replace('page=' . $_GET['page'], '', $urlPage);
+    } else {
+        $urlPage = $urlCurrent;
+    }
+    if (strpos($urlPage, '?') !== false) {
+        if (count($_GET) >= 1) {
+            $urlPage = $urlPage . '&page=';
+        } else {
+            $urlPage = $urlPage . 'page=';
+        }
+    } else {
+        $urlPage = $urlPage . '?page=';
+    }
+    
+    if(@$_GET['status']==1){
+        $mess= '<p class="text-success" style="padding-left: 1.5em;">Thêm mới dữ liệu thành công</p>';
+
+    }elseif(@$_GET['status']==2){
+        $mess= '<p class="text-success" style="padding-left: 1.5em;">Sửa dữ liệu thành công</p>';
+
+    }elseif(@$_GET['status']==3){
+
+        $mess= '<p class="text-success" style="padding-left: 1.5em;">Xóa dữ liệu thành công</p>';
+    }
+
+    setVariable('mess', @$mess);
+    setVariable('page', $page);
+    setVariable('totalPage', $totalPage);
+    setVariable('back', $back);
+    setVariable('next', $next);
+    setVariable('urlPage', $urlPage);
+    
+    setVariable('listData', $listData);
+}
+
+
+function addRestaurantAdmin($input)
+{
+    global $controller;
+    global $isRequestPost;
+    global $modelCategories;
+    global $metaTitleMantan;
+    global $session;
+    
+    $metaTitleMantan = 'Thông tin Cơ quan hành chính';
+
+
+    $modelRestaurant = $controller->loadModel('Restaurants');
+    $mess= '';
+
+    // lấy data edit
+    if(!empty($_GET['id'])){
+        $data = $modelRestaurant->get( (int) $_GET['id']);
+
+    }else{
+        $data = $modelRestaurant->newEmptyEntity();
+         $data->created = getdate()[0];
+    }
+
+
+    if ($isRequestPost) {
+        $dataSend = $input['request']->getData();
+
+        if(!empty($dataSend['name'])){
+            // tạo dữ liệu save
+            $data->name = @$dataSend['name'];
+            $data->address = @$dataSend['address'];
+            $data->phone = @$dataSend['phone'];
+            $data->email = @$dataSend['email'];
+            $data->image = @$dataSend['image'];
+            $data->introductory = @$dataSend['introductory'];
+            $data->latitude = @$dataSend['latitude'];
+            $data->longitude = @$dataSend['longitude'];
+            $data->image360 = @$dataSend['image360'];
+            $data->content = @$dataSend['content'];
+            $data->status = @$dataSend['status'];
+            $data->urlSlug = createSlugMantan(trim($dataSend['name']));
+
+            $modelRestaurant->save($data);
+
+            $mess= '<p class="text-success">Lưu dữ liệu thành công</p>';
+
+             if(!empty($_GET['id'])){
+                return $controller->redirect('/plugins/admin/tayho360-admin-restaurant-listRestaurantAdmin.php?status=2');
+            }else{
+                return $controller->redirect('/plugins/admin/tayho360-admin-restaurant-listRestaurantAdmin.php?status=1');
+            }
+            
+        }else{
+            $mess= '<p class="text-danger">Bạn chưa nhập tên mã QR</p>';
+        }
+    }
+
+
+
+    setVariable('data', $data);
+    setVariable('mess', $mess);
+}
+
+function deleteRestaurantAdmin($input){
+    global $controller;
+    $modelRestaurant = $controller->loadModel('Restaurants');
+    if(!empty($_GET['id'])){
+        $data = $modelRestaurant->get($_GET['id']);
+        
+        if($data){
+            $modelRestaurant->delete($data);
+        }
+    }
+
+    return $controller->redirect('/plugins/admin/tayho360-admin-restaurant-listRestaurantAdmin.php?status=3');
+}
  ?>
