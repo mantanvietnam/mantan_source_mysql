4501f8e1f25c43c01ac315d7a23068b0abe55a61
Deocomate <deocomate@GMAIL.COM>
2023-04-12 09:42:18 +0700

    Delete debug listdataa

--- themes/tayho360/vietnam360.php
+++ themes/tayho360/vietnam360.php
@@ -1,1 +1,100 @@
-<?php debug(@$listData); ?>
+<?php
+getHeader();
+?>
+
+<main>
+    <section id="page-banner-img">
+        <div class="banner-img">
+            <img src="../img/hinh-anh-ho-tay.jpg" alt="">
+        </div>
+    </section>
+    <section class="section-background-index">
+        <div class="container-fluid background-index">
+            <img src="../img/background-index.jpg" alt="">
+        </div>
+    </section>
+
+    <section id="title-categorty-360">
+        <div class="category-title">
+            <h1>Việt Nam 360</h1>
+
+        </div>
+    </section>
+
+    <section id="section-img-360">
+        <div class="container-img-360 container">
+            <div class="row">
+                <div class="col-lg-4 col-md-6 col-sm-6 col-12 item-img-360">
+                    <div class="img-360">
+                        <a href=""><img src="../img/thaianhimg/ha_noi.jpg" alt=""></a>
+                    </div>
+                </div>
+
+                <div class="col-lg-4 col-md-6 col-sm-6 col-12 item-img-360">
+                    <div class="img-360">
+                        <a href=""><img src="../img/thaianhimg/ha_noi.jpg" alt=""></a>
+                    </div>
+                </div>
+
+                <div class="col-lg-4 col-md-6 col-sm-6 col-12 item-img-360">
+                    <div class="img-360">
+                        <a href=""><img src="../img/thaianhimg/ha_noi.jpg" alt=""></a>
+                    </div>
+                </div>
+
+                <div class="col-lg-4 col-md-6 col-sm-6 col-12 item-img-360">
+                    <div class="img-360">
+                        <a href=""><img src="../img/thaianhimg/ha_noi.jpg" alt=""></a>
+                    </div>
+                </div>
+
+                <div class="col-lg-4 col-md-6 col-sm-6 col-12 item-img-360">
+                    <div class="img-360">
+                        <a href=""><img src="../img/thaianhimg/ho_chi_minh.jpg" alt=""></a>
+                    </div>
+                </div>
+
+                <div class="col-lg-4 col-md-6 col-sm-6 col-12 item-img-360">
+                    <div class="img-360">
+                        <a href=""><img src="../img/thaianhimg/ho_chi_minh.jpg" alt=""></a>
+                    </div>
+                </div>
+
+                <div class="col-lg-4 col-md-6 col-sm-6 col-12 item-img-360">
+                    <div class="img-360">
+                        <a href=""><img src="../img/thaianhimg/ho_chi_minh.jpg" alt=""></a>
+                    </div>
+                </div>
+
+                <div class="col-lg-4 col-md-6 col-sm-6 col-12 item-img-360">
+                    <div class="img-360">
+                        <a href=""><img src="../img/thaianhimg/ho_chi_minh.jpg" alt=""></a>
+                    </div>
+                </div>
+
+                <div class="col-lg-4 col-md-6 col-sm-6 col-12 item-img-360">
+                    <div class="img-360">
+                        <a href=""><img src="../img/thaianhimg/ha_noi.jpg" alt=""></a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <section id="pagination-page">
+        <nav aria-label="Page navigation example">
+            <ul class="pagination">
+                <li class="page-item"><a class="page-link" href="#"><i class="fa-solid fa-chevron-left"></i></a>
+                </li>
+                <li class="page-item "><a class="page-link active" href="#">1</a></li>
+                <li class="page-item"><a class="page-link" href="#">2</a></li>
+                <li class="page-item"><a class="page-link" href="#">3</a></li>
+                <li class="page-item"><a class="page-link" href="#"><i class="fa-solid fa-chevron-right"></i></a>
+                </li>
+            </ul>
+        </nav>
+    </section>
+</main>
+
+<?php
+getFooter();
