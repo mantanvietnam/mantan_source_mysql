8b2190732096222ec46006e75098a18ee913965f
Trần Mạnh <tranmanhbk179@gmail.com>
2023-06-19 18:20:14 +0700

    Update createImageSeries.php

--- Plugin/ezpics_designer/view/home/product/createImageSeries.php
+++ Plugin/ezpics_designer/view/home/product/createImageSeries.php
@@ -22,10 +22,48 @@ if(!empty($_GET['id'])){
 				<meta name="viewport" content="width=device-width, initial-scale=1">
 				<title>Hình ảnh Ezpics</title>
 
+				<script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap/5.2.3/js/bootstrap.min.js" integrity="sha512-1/RvZTcCDEUjY/CypiMz+iqqtaoQfAITmNSJY17Myp4Ms5mdxPS5UV7iOfdZoxcGhzFbOm6sntTKJppjvuhg4g==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+
+				<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap/5.2.3/css/bootstrap.min.css" integrity="sha512-SbiR/eusphKoMVVXysTKG/7VseWii+Y3FdHrt0EpKgpToZeemhqHeZeLWLhJutz/2ut2Vw1uQEj2MbRF+TVBUA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+
 				<link rel="icon" type="image/x-icon" href="https://designer.ezpics.vn/plugins/ezpics_designer/view/home/assets/img/avatar-ezpics.png" />
 			</head>
 			<body>
-				<img src="data:image/png;base64,'.$dataImage.'" width="100%" />
+				<p class="text-center">
+					<button type="button" class="btn btn-warning mb-2 mt-3" onclick="downloadImage();">
+					  Tải ảnh
+					</button>
+				</p>
+				<img id="imageId" src="data:image/png;base64,'.$dataImage.'" width="100%" />
+
+				<script type="text/javascript">
+					var slug= "'.$slug.'";
+
+					function downloadImage()
+					{
+						// Truy cập đến phần tử hình ảnh
+						var imageElement = document.getElementById("imageId");
+
+						// Lấy đường dẫn (URL) của hình ảnh
+						var imageURL = imageElement.src;
+
+						// Tạo đối tượng anchor
+						var a = document.createElement("a");
+						a.href = imageURL;
+
+						// Thiết lập thuộc tính download cho anchor
+						a.download = slug+".jpg";
+
+						// Gắn anchor vào phần tử body
+						document.body.appendChild(a);
+
+						// Simulate click để tải xuống
+						a.click();
+
+						// Xóa anchor sau khi tải xuống
+						document.body.removeChild(a);
+					}
+				</script>
 			</body>
 			</html>';
 }
