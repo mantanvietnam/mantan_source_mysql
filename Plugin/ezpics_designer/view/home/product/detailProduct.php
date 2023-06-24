23138cfc68d19f583ef5db41b245febf951b6b3a
hieugiataytay <hieuugia20@gmail.com>
2023-06-24 15:40:01 +0700

    update

--- Plugin/ezpics_designer/view/home/product/detailProduct.php
+++ Plugin/ezpics_designer/view/home/product/detailProduct.php
@@ -1,104 +1,4 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-   <?php mantan_header();?>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
-    <link rel="icon" type="image/x-icon" href="https://designer.ezpics.vn/plugins/ezpics_designer/view/home/assets/img/avatar-ezpics.png" />
-
-    <link rel="stylesheet" href="/plugins/ezpics_designer/view/home/designer/assets/css/style.css?time=<?php echo  getdate()[0]; ?>">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js" integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossorigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.min.js" integrity="sha384-cVKIPhGWiC2Al4u+LWgxfKTRIcfu0JTxR+EQDz/bgldoEyl4H0zUF0QKbrJ0EcQF" crossorigin="anonymous"></script>
-    <link rel="stylesheet" type="text/css"  href="https://cdn.jsdelivr.net/npm/slick-carousel@1.8.1/slick/slick.css" />
-      <script>
-	  window.dataLayer = window.dataLayer || [];
-	  function gtag(){dataLayer.push(arguments);}
-	  gtag('js', new Date());
-
-	  gtag('config', 'G-PCQ02R5K9G');
-	</script>
-</head>
-<body>
-<header>
-    <section class="section-header">
-        <nav class="navbar navbar-expand-lg navbar-light bg-light">
-            <div class="container-fluid header-menu-mobile">
-                <button class="button-header">
-                    <i class="fa-solid fa-bars"></i>
-                </button>
-
-                <div class="logo-header">
-                        <a href="#"><img src="https://designer.ezpics.vn/plugins/ezpics_designer/view/home/assets/img/avatar-ezpics.png" alt=""></a>
-                </div>
-
-                <div class="menu-mobile">
-                    <ul class="navbar-nav mb-2 mb-lg-0">
-                        <li class="nav-item">
-                            <a class="nav-link" href="https://ezpics.vn/tinh-nang/">TÍNH NĂNG</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="https://ezpics.vn/mau-thiet-ke-noi-bat/">MẪU THIẾT KẾ NỔI BẬT</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="https://huongdan.ezpics.vn/">HƯỚNG DẪN SỬ DỤNG</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="https://ezpics.vn/category/blog/">BLOG</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="https://ezpics.vn/lien-he/">LIÊN HỆ</a>
-                        </li>
-                    </ul>
-                </div>
-            </div>
-
-            <div class="container-fluid">
-                <div class="navbar-header collapse navbar-collapse" id="navbarTogglerDemo01">
-                <div class="col-5 header-menu">
-                    <ul class="navbar-nav mb-2 mb-lg-0">
-                    <li class="nav-item">
-                        <a class="nav-link" href="https://ezpics.vn/tinh-nang/">TÍNH NĂNG</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="https://ezpics.vn/mau-thiet-ke-noi-bat/">MẪU THIẾT KẾ NỔI BẬT</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="https://huongdan.ezpics.vn/">HƯỚNG DẪN SỬ DỤNG</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="https://ezpics.vn/category/blog/">BLOG</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="https://ezpics.vn/lien-he/">LIÊN HỆ</a>
-                    </li>
-                    </ul>
-                </div>
-
-                <div class="col-2">
-                    <div class="logo-header">
-                        <a href="#"><img src="https://designer.ezpics.vn/plugins/ezpics_designer/view/home/assets/img/avatar-ezpics.png" alt=""></a>
-                    </div>
-                </div>
-                
-
-                <div class="col-5 header-search">
-                    
-                    <form class="d-flex">
-                        <a class="download-button-header" href="https://huongdan.ezpics.vn/tai-app-ezpics">TẢI EZPICS MIỄN PHÍ</a>
-                        <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
-                        <button class="btn btn-outline-success" type="submit"><i class="fa-solid fa-magnifying-glass"></i></button>
-                    </form>
-                </div>
-                </div>
-            </div>
-            </nav>
-    </section>
-</header>
-
-
+<?php include(__DIR__.'/../headerPublic.php') ; ?>
     <main>
       
         <?php
@@ -126,14 +26,15 @@
                     <div class="col-lg-5 col-md-5 col-12 product-information">
                         <h1 class="product-title"><?php echo $product->name ?></h1>
                         <div>
-                            <p>Tác giả: <span><?php echo $user->name ?></span></p>
+                            <p>Tác giả: <a class="product-name-user" href="<?php echo $user->link_open_app ; ?>"><?php echo $user->name ?></a></p>
                             <p>Lượt xem: <span><?php echo $product->views ?></span></p>
                             <p>Đã bán: <span> <?php echo $product->sold ?></span></p>
                             <div class="price-product">
                                 <p>Giá bán: <span><?php echo $sale_price  ?></span></p>
                             </div>
                             <?php if(!empty($description)){ ?>
-                            <p>Mô tả: <span><?php echo $description ?></span></p>
+                            <br>
+                            <span><?php echo nl2br($description); ?></span>
                             <?php } ?>
                         </div>
                         <br>
@@ -178,7 +79,7 @@
 	                                    <p><?php echo @$item->name ?></p>
 	                                </div>
 	                                <div class="product-sold">
-	                                    <p>Đã bán :<span><?php echo @$item->sold ?></span></p>
+	                                    <p>Đã bán: <span><?php echo @$item->sold ?></span></p>
 	                                </div>
 	                                <div class="product-price">
 	                                    <?php echo $price ?>
@@ -192,76 +93,8 @@
         </section>
         
     </main>
-    
-    <footer>
-        <section id="section-footer">
-            <div class="container">
-                <div class="row">
-                    <div class="col-12 col-lg-5 col-md-5 footer-left">
-                        <div class="logo-footer">
-                            <img src="https://designer.ezpics.vn/plugins/ezpics_designer/view/home/assets/img/avatar-ezpics.png" alt="">
-                        </div>
 
-                        <div class="downloadapp-footer">
-                            <div class="downloadapp-footer-title">
-                                <p>Tải ứng dụng miễn phí</p>
-                            </div>
-
-                            <div class="downloadapp-footer-item">
-                                <a href="https://apps.apple.com/vn/app/ezpics-kho-m%E1%BA%ABu-thi%E1%BA%BFt-k%E1%BA%BF-%E1%BA%A3nh/id1659195883?l=vi"><img src="/plugins/ezpics_designer/view/home/designer/assets/img/logo-appstore-download-300x95.png" alt=""></a>
-                                <a href="https://play.google.com/store/apps/details?id=vn.ezpics&hl=vi&gl=US"><img src="/plugins/ezpics_designer/view/home/designer/assets/img/Logo-google-play-store-e1578969817208-300x95.png" alt=""></a>
-                            </div>
-                        </div>
-                    </div>
-
-                    <div class="col-12 col-lg-3 col-md-3 footer-right">
-                        <div class="menu-footer menu-footer-one">
-                            <ul>
-                                <p>Công cụ thiết kế ảnh</p>
-                                <li>Thiết kế Banner - Poster độc đáo</li>
-                                <li>Dành cho lập trình viên</li>
-                                <li>Dành cho Nhà Thiết Kế</li>
-                                <li>Giải pháp doanh nghiệp</li>
-                            </ul>
-                        </div>
-                    </div>
-
-                    <div class="col-12 col-lg-3 col-md-3 footer-right">
-                        <div class="menu-footer menu-footer-two">
-                            <ul>
-                                <p>GIỚI THIỆU</p>
-                                <li>Giới thiệu</li>
-                                <li>Blog</li>
-                                <li>Công việc</li>
-                                <li>Nhãn hiệu</li>
-                                <li>Thông tin truyền thông</li>
-                            </ul>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </section>
-
-        <section id="section-footer-bottom">
-            <div class="container footer-bottom-container">
-                <div class="footer-bottom-link">
-                    <a>Điều khoản sử dụng</a>
-                    <a>Chính sách quyền riêng tư</a>
-                    <a>Chính sách bảo mật</a>
-                </div>
-
-                <div class="footer-bottom-copyright">
-                    <p>@2023 Ezpics.vn</p>
-                </div>
-            </div>
-        </section>
- 
-   <script type="text/javascript" src="https://code.jquery.com/jquery-1.11.0.min.js"></script>
-    <script type="text/javascript" src="https://code.jquery.com/jquery-migrate-1.2.1.min.js"></script>
     <script type="text/javascript" src="https://cdn.jsdelivr.net/npm/slick-carousel@1.8.1/slick/slick.min.js"></script>
     <script type="text/javascript" src="/plugins/ezpics_designer/view/home/designer/assets/js/slick.js?time=<?php echo  getdate()[0]; ?>"></script>
-
-</footer>
-</body>
-
-</html>
+    
+    <?php include(__DIR__.'/../footerPublic.php') ; ?>
--- Plugin/ezpics_designer/view/home/warehouse/detailWarehouse.php
+++ Plugin/ezpics_designer/view/home/warehouse/detailWarehouse.php
@@ -1,96 +1,4 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <link rel="icon" type="image/x-icon" href="https://designer.ezpics.vn/plugins/ezpics_designer/view/home/assets/img/avatar-ezpics.png" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">  
-    <?php mantan_header();?>
-    <link rel="stylesheet" href="/plugins/ezpics_designer/view/home/designer/assets/css/style.css?time=<?php echo  getdate()[0]; ?>">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js" integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossorigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.min.js" integrity="sha384-cVKIPhGWiC2Al4u+LWgxfKTRIcfu0JTxR+EQDz/bgldoEyl4H0zUF0QKbrJ0EcQF" crossorigin="anonymous"></script>
-
-</head>
-<body>
-    <header>
-
-        <div class="container-fluid header-menu-mobile">
-            <button class="button-header">
-                <i class="fa-solid fa-bars"></i>
-            </button>
-
-            <div class="logo-header">
-                <a href="#"><img src="https://designer.ezpics.vn/plugins/ezpics_designer/view/home/assets/img/avatar-ezpics.png" alt=""></a>
-            </div>
-
-            <div class="menu-mobile">
-                <ul class="navbar-nav mb-2 mb-lg-0">
-                    <li class="nav-item">
-                        <a class="nav-link" href="https://ezpics.vn/tinh-nang/">TÍNH NĂNG</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="https://ezpics.vn/mau-thiet-ke-noi-bat/">MẪU THIẾT KẾ NỔI BẬT</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="https://huongdan.ezpics.vn/">HƯỚNG DẪN SỬ DỤNG</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="https://ezpics.vn/category/blog/">BLOG</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="https://ezpics.vn/lien-he/">LIÊN HỆ</a>
-                    </li>
-                </ul>
-            </div>
-        </div>
-
-
-        <section class="section-header">
-            <nav class="navbar navbar-expand-lg navbar-light bg-light">
-                <div class="container-fluid">
-                    <div class="navbar-header collapse navbar-collapse" id="navbarTogglerDemo01">
-                    <div class="col-5 header-menu">
-                        <ul class="navbar-nav mb-2 mb-lg-0">
-                        <li class="nav-item">
-                            <a class="nav-link" href="https://ezpics.vn/tinh-nang/">TÍNH NĂNG</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="https://ezpics.vn/mau-thiet-ke-noi-bat/">MẪU THIẾT KẾ NỔI BẬT</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="https://huongdan.ezpics.vn/">HƯỚNG DẪN SỬ DỤNG</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="https://ezpics.vn/category/blog/">BLOG</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="https://ezpics.vn/lien-he/">LIÊN HỆ</a>
-                        </li>
-                        </ul>
-                    </div>
-
-                    <div class="col-2">
-                        <div class="logo-header">
-                            <a href="#"><img src="https://designer.ezpics.vn/plugins/ezpics_designer/view/home/assets/img/avatar-ezpics.png" alt=""></a>
-                        </div>
-                    </div>
-                    
-
-                    <div class="col-5 header-search">
-                        
-                        <form class="d-flex">
-                            <a class="download-button-header" href="https://huongdan.ezpics.vn/tai-app-ezpics">TẢI EZPICS MIỄN PHÍ</a>
-                            <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
-                            <button class="btn btn-outline-success" type="submit"><i class="fa-solid fa-magnifying-glass"></i></button>
-                        </form>
-                    </div>
-                    </div>
-                </div>
-                </nav>
-        </section>
-    </header>
+<?php include(__DIR__.'/../headerPublic.php') ; ?>
     <main>
         <section id="designer-background">
             <img src="/plugins/ezpics_designer/view/home/designer/assets/img/banner.png" alt="">
@@ -258,99 +166,32 @@
         </section>
         
     </main>
-    <footer>
-        <section id="section-footer">
-            <div class="container">
-                <div class="row">
-                    <div class="col-12 col-lg-5 col-md-5 footer-left">
-                        <div class="logo-footer">
-                            <img src="https://designer.ezpics.vn/plugins/ezpics_designer/view/home/assets/img/avatar-ezpics.png" alt="">
-                        </div>
+    <script type="text/javascript">
+        function copyToClipboard(textCopy,messId) {
+            // Create a "hidden" input
+            var aux = document.createElement("input");
 
-                        <div class="downloadapp-footer">
-                            <div class="downloadapp-footer-title">
-                                <p>Tải ứng dụng miễn phí</p>
-                            </div>
+            // Assign it the value of the specified element
+            aux.setAttribute("value", textCopy);
 
-                            <div class="downloadapp-footer-item">
-                                <a href="https://apps.apple.com/vn/app/ezpics-kho-m%E1%BA%ABu-thi%E1%BA%BFt-k%E1%BA%BF-%E1%BA%A3nh/id1659195883?l=vi"><img src="/plugins/ezpics_designer/view/home/designer/assets/img/logo-appstore-download-300x95.png" alt=""></a>
-                                <a href="https://play.google.com/store/apps/details?id=vn.ezpics&hl=vi&gl=US"><img src="/plugins/ezpics_designer/view/home/designer/assets/img/Logo-google-play-store-e1578969817208-300x95.png" alt=""></a>
-                            </div>
-                        </div>
-                    </div>
+            // Append it to the body
+            document.body.appendChild(aux);
 
-                    <div class="col-12 col-lg-3 col-md-3 footer-right">
-                        <div class="menu-footer menu-footer-one">
-                            <ul>
-                                <p>Công cụ thiết kế ảnh</p>
-                                <li>Thiết kế Banner - Poster độc đáo</li>
-                                <li>Dành cho lập trình viên</li>
-                                <li>Dành cho Nhà Thiết Kế</li>
-                                <li>Giải pháp doanh nghiệp</li>
-                            </ul>
-                        </div>
-                    </div>
+            // Highlight its content
+            aux.select();
 
-                    <div class="col-12 col-lg-3 col-md-3 footer-right">
-                        <div class="menu-footer menu-footer-two">
-                            <ul>
-                                <p>GIỚI THIỆU</p>
-                                <li>Giới thiệu</li>
-                                <li>Blog</li>
-                                <li>Công việc</li>
-                                <li>Nhãn hiệu</li>
-                                <li>Thông tin truyền thông</li>
-                            </ul>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </section>
+            // Copy the highlighted text
+            document.execCommand("copy");
 
-        <section id="section-footer-bottom">
-            <div class="container footer-bottom-container">
-                <div class="footer-bottom-link">
-                    <a>Điều khoản sử dụng</a>
-                    <a>Chính sách quyền riêng tư</a>
-                    <a>Chính sách bảo mật</a>
-                </div>
+            // Remove it from the body
+            document.body.removeChild(aux);
 
-                <div class="footer-bottom-copyright">
-                    <p>@2023 Ezpics.vn</p>
-                </div>
-            </div>
-        </section>
- 
-   <script type="text/javascript" src="https://code.jquery.com/jquery-1.11.0.min.js"></script>
-    <script type="text/javascript" src="https://code.jquery.com/jquery-migrate-1.2.1.min.js"></script>
+            // show mess
+            alert('bạn đã sao chép link chia sẻ');
+
+        }
+    </script>
     <script type="text/javascript" src="https://cdn.jsdelivr.net/npm/slick-carousel@1.8.1/slick/slick.min.js"></script>
     <script type="text/javascript" src="/plugins/ezpics_designer/view/home/designer/assets/js/slick.js?time=<?php echo  getdate()[0]; ?>"></script>
 
-</footer>
-</body>
-<script type="text/javascript">
-  function copyToClipboard(textCopy,messId) {
-    // Create a "hidden" input
-    var aux = document.createElement("input");
-
-    // Assign it the value of the specified element
-    aux.setAttribute("value", textCopy);
-
-    // Append it to the body
-    document.body.appendChild(aux);
-
-    // Highlight its content
-    aux.select();
-
-    // Copy the highlighted text
-    document.execCommand("copy");
-
-    // Remove it from the body
-    document.body.removeChild(aux);
-
-    // show mess
-    alert('bạn đã sao chép link chia sẻ');
-
-}
-</script>
-</html>
+    <?php include(__DIR__.'/../footerPublic.php') ; ?>
