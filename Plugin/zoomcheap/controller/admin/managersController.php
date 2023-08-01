eb42bdfc6bc28156ecc8f3884c79fa568797a441
hieugiataytay <hieuugia20@gmail.com>
2023-08-02 03:20:19 +0700

    update

---
+++ Plugin/zoomcheap/controller/admin/managersController.php
@@ -0,0 +1,79 @@
+<?php 
+function listManagerAdmin($input)
+{
+	global $controller;
+	global $urlCurrent;
+	global $modelCategories;
+    global $metaTitleMantan;
+
+    $metaTitleMantan = 'Danh sách khách hàng';
+
+	$modelManagers = $controller->loadModel('Managers');
+
+	$conditions = array();
+	$limit = 20;
+	$page = (!empty($_GET['page']))?(int)$_GET['page']:1;
+	if($page<1) $page = 1;
+    $order = array('id'=>'desc');
+
+    if(!empty($_GET['id'])){
+        $conditions['id'] = (int) $_GET['id'];
+    }
+
+    if(!empty($_GET['fullname'])){
+        $conditions['fullname LIKE'] = '%'.$_GET['fullname'].'%';
+    }
+
+    if(!empty($_GET['phone'])){
+        $conditions['phone'] = $_GET['phone'];
+    }
+
+    if(!empty($_GET['email'])){
+        $conditions['email'] = $_GET['email'];
+    }
+    
+    $listData = $modelManagers->find()->limit($limit)->page($page)->where($conditions)->order($order)->all()->toList();
+
+    // phân trang
+    $totalData = $modelManagers->find()->where($conditions)->all()->toList();
+    $totalData = count($totalData);
+
+    $balance = $totalData % $limit;
+    $totalPage = ($totalData - $balance) / $limit;
+    if ($balance > 0)
+        $totalPage+=1;
+
+    $back = $page - 1;
+    $next = $page + 1;
+    if ($back <= 0)
+        $back = 1;
+    if ($next >= $totalPage)
+        $next = $totalPage;
+
+    if (isset($_GET['page'])) {
+        $urlPage = str_replace('&page=' . $_GET['page'], '', $urlCurrent);
+        $urlPage = str_replace('page=' . $_GET['page'], '', $urlPage);
+    } else {
+        $urlPage = $urlCurrent;
+    }
+    if (strpos($urlPage, '?') !== false) {
+        if (count($_GET) >= 1) {
+            $urlPage = $urlPage . '&page=';
+        } else {
+            $urlPage = $urlPage . 'page=';
+        }
+    } else {
+        $urlPage = $urlPage . '?page=';
+    }
+
+
+    setVariable('page', $page);
+    setVariable('totalPage', $totalPage);
+    setVariable('back', $back);
+    setVariable('next', $next);
+    setVariable('urlPage', $urlPage);    
+    setVariable('listData', $listData);
+
+}
+
+?>
