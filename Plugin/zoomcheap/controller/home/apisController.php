9cb8596f2f7ec908c1cf64293ac30604fc1e2a8e
Trần Mạnh <tranmanhbk179@gmail.com>
2023-08-04 21:05:38 +0700

    update

--- Plugin/zoomcheap/controller/home/apisController.php
+++ Plugin/zoomcheap/controller/home/apisController.php
@@ -74,3 +74,103 @@ function checkDeadlineOrderAPI($input)
 
 	return ['number_extend'=>$number_extend,'number_deadline'=>$number_deadline];
 }
+
+function addMoneyTPBankAPI($input)
+{
+	global $controller;
+
+	$return['messages']= array(array('text'=>''));
+
+	$modelManagers = $controller->loadModel('Managers');
+	$modelHistories = $controller->loadModel('Histories');
+
+	if(!empty($_POST['message'])){
+
+	 	$keyApp= 'THUEZOOM';
+
+		$message = strtoupper($_POST['message']);
+
+		$description = explode('ND: ', $message);
+		$description = trim($description[1]);
+		$description = str_replace(array('IBFT ','THANH TOAN QR ','QR - '), '', $description);
+
+		$money = explode('PS:+', $message);
+		$money = explode('SD:', $money[1]);
+		$money = (int) str_replace(array('.','VND'), '', $money[0]);
+
+		if($money>0 && strlen(strstr($description, $keyApp)) > 0){
+			// xóa dấu chấm
+			$removeDot = explode('.', $description);
+			if(count($removeDot)>1){
+				for($i=0;$i<count($removeDot);$i++){
+					if(strlen(strstr($removeDot[$i], $keyApp)) > 0){
+						$description = $removeDot[$i];
+						break;
+					}
+				}
+			}
+
+			// xóa dấu chấm phẩy
+			$removeDot = explode(';', $description);
+			if(count($removeDot)>1){
+				for($i=0;$i<count($removeDot);$i++){
+					if(strlen(strstr($removeDot[$i], $keyApp)) > 0){
+						$description = $removeDot[$i];
+						break;
+					}
+				}
+			}
+
+			// xóa dấu gạch ngang
+			$removeDot = explode('-', $description);
+			if(count($removeDot)>1){
+				for($i=0;$i<count($removeDot);$i++){
+					if(strlen(strstr($removeDot[$i], $keyApp)) > 0){
+						$description = $removeDot[$i];
+						break;
+					}
+				}
+			}
+
+
+			$removeSpace = explode(' ', trim($description));
+			$phone = $removeSpace[0];
+			$phone= str_replace(array(' ','.','-'), '', $phone);
+			$phone = str_replace('+84','0',$phone);
+
+			$infoUser = $modelManagers->find()->where(['phone'=>$phone])->first();
+
+			if(!empty($infoUser)){
+				// cộng tiền tài khoản
+				$infoUser->coin += $money;
+				$modelManagers->save($infoUser);
+
+				// lưu lịch sử giao dịch
+				$dataHistories = $modelHistories->newEmptyEntity();
+
+		        $dataHistories->time = time();
+		        $dataHistories->idManager = $infoUser->id;
+		        $dataHistories->numberCoin = $money;
+		        $dataHistories->numberCoinManager = $infoUser->coin;
+		        $dataHistories->type = 'plus';
+		        $dataHistories->note = 'Nạp tiền tài khoản qua chuyển khoản';
+		        $dataHistories->type_note = 'plus_banking';
+		        $dataHistories->modified = time();
+		        $dataHistories->created = time();
+
+		        $modelHistories->save($dataHistories);
+			}else{
+				$mess = 'Không tìm thấy tài khoản khách hàng';
+			}
+
+			$return['messages']= array(array('text'=>$mess));
+		} else {
+			$return['messages']= array(array('text'=>'Sai cú pháp hoặc số tiền không đủ'));
+		}
+   	 	
+	}else{
+		$return['messages']= array(array('text'=>'Gửi thiếu nội dung SMS'));
+	}
+
+	return $return;
+}
