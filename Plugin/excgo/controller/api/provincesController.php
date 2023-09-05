d65c6fbfc5f10216ae56a11960c9d3b98f0bc617
quang-nguyen-0092 <quang.nguyen@tda.company>
2023-09-05 16:42:13 +0700

    Feat: api craate/remove pinned provinces

--- Plugin/excgo/controller/api/provincesController.php
+++ Plugin/excgo/controller/api/provincesController.php
@@ -3,40 +3,67 @@
 function getListProvinceApi($input): array
 {
     global $controller;
+    global $isRequestPost;
 
     $modelProvinces = $controller->loadModel('Provinces');
-    $conditions = array();
-    $limit = (!empty($_GET['limit'])) ? (int)$_GET['limit'] : 20;
-    $page = (!empty($_GET['page'])) ? (int)$_GET['page'] : 1;
-    if ($page < 1) $page = 1;
 
-    if (!empty($_GET['name'])) {
-        $conditions['name LIKE'] = '%' . $_GET['name'] . '%';
+    if ($isRequestPost) {
+        $dataSend = $input['request']->getData();
+
+        if (!isset($dataSend['access_token'])) {
+            return apiResponse(3, 'Tài khoản không tồn tại hoặc sai mã token');
+        } else {
+            $currentUser = getUserByToken($dataSend['access_token']);
+
+            if (empty($currentUser)) {
+                return apiResponse(3, 'Tài khoản không tồn tại hoặc sai mã token');
+            }
+        }
+
+        $conditions = ['status' => 1];
+        $limit = (!empty($dataSend['limit'])) ? $dataSend['limit'] : 20;
+        $page = (!empty($dataSend['page'])) ? $dataSend['page'] : 1;
+        if ($page < 1) $page = 1;
+
+        if (!empty($dataSend['name'])) {
+            $conditions['name LIKE'] = '%' . $dataSend['name'] . '%';
+        }
+
+        if (!empty($dataSend['bsx'])) {
+            $conditions['bsx LIKE'] = '%' . $dataSend['bsx'] . '%';
+        }
+
+        $listProvince = $modelProvinces->find()
+            ->join([
+                'table' => 'pinned_provinces',
+                'alias' => 'PinnedProvinces',
+                'type' => 'LEFT',
+                'conditions' => [
+                    'PinnedProvinces.province_id = Provinces.id',
+                    'PinnedProvinces.user_id' => $currentUser->id
+                ],
+            ])->where($conditions)
+            ->limit($limit)
+            ->page($page)
+            ->order([
+                'PinnedProvinces.id IS NULL' => 'ASC', // Tỉnh nào được ghim sẽ sắp xếp lên trước
+                'PinnedProvinces.created_at' => 'DESC', // Tỉnh nào được ghim gần nhất xếp lên trước
+                'Provinces.name' => 'ASC' // Các tỉnh còn lại sắp xếp theo tên
+            ])->all()
+            ->toList();
+
+        $totalProvince = $modelProvinces->find()
+            ->where($conditions)
+            ->all()
+            ->toList();
+        $paginationMeta = createPaginationMetaData(
+            count($totalProvince),
+            $limit,
+            $page
+        );
+
+        return apiResponse(0, 'Lấy danh sách tỉnh thành công', $listProvince, $paginationMeta);
     }
 
-    if (!empty($_GET['bsx'])) {
-        $conditions['bsx LIKE'] = '%' . $_GET['bsx'] . '%';
-    }
-
-    if (isset($_GET['status']) && $_GET['status'] !== '') {
-        $conditions['status'] = $_GET['status'];
-    }
-
-    $listData = $modelProvinces->find()
-        ->limit($limit)
-        ->page($page)
-        ->where($conditions)
-        ->all()
-        ->toList();
-    $totalProvince = $modelProvinces->find()
-        ->where($conditions)
-        ->all()
-        ->toList();
-    $paginationMeta = createPaginationMetaData(
-        count($totalProvince),
-        $limit,
-        $page
-    );
-
-    return apiResponse(0, 'Lấy danh sách tỉnh thành công', $listData, $paginationMeta);
+    return apiResponse(1, 'Bắt buộc sử dụng phương thức POST');
 }
