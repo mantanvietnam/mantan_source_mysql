1d492681b2b9e87d24a529077d5142f8705c69ff
hieugiataytay <hieuugia20@gmail.com>
2023-10-10 09:16:44 +0700

    update

--- themes/warm/header.php
+++ themes/warm/header.php
@@ -18,6 +18,7 @@ https://cdn.jsdelivr.net/npm/odometer@0.4.8/themes/odometer-theme-default.min.cs
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
     <link rel="stylesheet" href="<?php echo $urlThemeActive;?>/asset/css/main.css">
     <link rel="stylesheet" href="<?php echo $urlThemeActive;?>/asset/css/mainPlus.css">
+    <link rel="stylesheet" href="<?php echo $urlThemeActive;?>/asset/css/style.css">
 
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
     <!-- boostrap -->
@@ -43,6 +44,9 @@ https://cdn.jsdelivr.net/npm/odometer@0.4.8/themes/odometer-theme-default.min.cs
     <link rel="stylesheet" href="https://unpkg.com/aos@next/dist/aos.css" />
 
     <!-- chay so -->    
+    <!-- swipper -->
+    <script src="https://cdn.jsdelivr.net/npm/swiper@10/swiper-bundle.min.js"></script>
+
 </head>
 <body>
     <header>
