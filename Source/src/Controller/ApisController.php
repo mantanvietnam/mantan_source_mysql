813923463854e6f6c2b04ff68240db043200329c
Trần Mạnh <tranmanhbk179@gmail.com>
2023-10-24 18:52:50 +0700

    Update ApisController.php

--- Source/src/Controller/ApisController.php
+++ Source/src/Controller/ApisController.php
@@ -34,6 +34,8 @@ class ApisController extends AppController{
         $this->response = $this->response->withStringBody(json_encode($content));
 		$this->response = $this->response->withType('json');
 		$this->response = $this->response->withHeader('Access-Control-Allow-Origin', '*');
+		$this->response = $this->response->withHeader('Access-Control-Allow-Headers', '*');
+		$this->response = $this->response->withHeader('Access-Control-Allow-Methods', '*');
 
 		return $this->response;
 	}
