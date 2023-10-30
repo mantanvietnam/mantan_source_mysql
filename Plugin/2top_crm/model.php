a7bd5b890a99adfb201792598d9922ba0657e429
huubang97 <69882867+huubang97@users.noreply.github.com>
2023-10-30 20:57:29 +0700

    update

--- Plugin/2top_crm/model.php
+++ Plugin/2top_crm/model.php
@@ -1,9 +1,5 @@
-<<<<<<< HEAD
+
 <?php 
 include('model/customer.php');
 include('model/address.php');
-=======
-<?php 
-include('model/customer.php');
->>>>>>> 6785001b100f8bb02bb341dc5052d1d65e981c5b
 ?>
