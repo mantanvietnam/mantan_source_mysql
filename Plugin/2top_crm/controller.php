cf53ecbf12590f4ba7a25f264033fdfd775f071f
VanTung <vantungdeveloper@gmail.com>
2023-10-30 18:07:22 +0700

    update

--- Plugin/2top_crm/controller.php
+++ Plugin/2top_crm/controller.php
@@ -1,6 +1,5 @@
 <?php 
 include_once('controller/customersController.php'); // khách hàng
-include_once('controller/addressController.php'); // khách hàng
 include_once('controller/apiController.php'); // API
 include_once('controller/homesController.php'); // frontend
 ?>
