b09b788e9ff202a6aa930c951ebc72441734c27c
hieugiataytay <hieuugia20@gmail.com>
2023-10-31 21:47:03 +0700

    update

--- themes/shopbanhang/header.php
+++ themes/shopbanhang/header.php
@@ -15,6 +15,8 @@ $infoUser = $session->read('infoUser');
     <link rel="stylesheet" href="<?php echo $urlThemeActive ?>asset/css/cssplus.css">
     <link rel="stylesheet" href="<?php echo $urlThemeActive ?>asset/css/mainplus.css">
     <link rel="stylesheet" href="<?php echo $urlThemeActive ?>asset/css/review.css">
+    <link rel="stylesheet" href="<?php echo $urlThemeActive ?>asset/css/editcss.css">
+
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
     <!-- boostrap -->
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
