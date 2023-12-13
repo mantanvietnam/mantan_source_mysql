1e0f1e90f19981180759cfcd5dc6dfd0d37f52ab
huubang97 <69882867+huubang97@users.noreply.github.com>
2023-12-13 10:52:23 +0700

    update

--- themes/maichau360/footer.php
+++ themes/maichau360/footer.php
@@ -56,7 +56,7 @@ $setting = setting();
     <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
     <script type="text/javascript" src="https://code.jquery.com/jquery-migrate-1.2.1.min.js"></script>
     <script type="text/javascript" src="https://cdn.jsdelivr.net/npm/slick-carousel@1.8.1/slick/slick.min.js"></script>
-    <script src="<?= $urlThemeActive ?>js/main.js"></script>
+    <script src="<?= $urlThemeActive ?>maichau/js/main.js"></script>
 
 
     <div class="scripts">
