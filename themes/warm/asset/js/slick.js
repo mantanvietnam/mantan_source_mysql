6c8727f8f6dcf7b493b805646b2d2dfa5930a4bc
hieugiataytay <hieuugia20@gmail.com>
2024-01-14 04:05:00 +0700

    up

--- themes/warm/asset/js/slick.js
+++ themes/warm/asset/js/slick.js
@@ -270,7 +270,32 @@ $('.list-partner').slick({
   
   });
     
+// Bo sung
 
+$('.slide-ourproject-video-inner').slick({
+  infinite: true,
+  slidesToShow: 1,
+  slidesToScroll: 1,
+  arrows:true,
+  prevArrow:
+  `<button type='button' class='slick-arrow slick-prev pull-left'><i class="fa-solid fa-chevron-left"></i></button>`,
+  nextArrow:
+  `<button type='button' class='slick-arrow slick-next pull-right'><i class="fa-solid fa-chevron-right"></i></button>`,
+});
+
+
+$('.ourproject-slide-top').slick({
+  infinite: true,
+  slidesToShow: 1,
+  slidesToScroll: 1,
+  arrows:true,
+  prevArrow:
+  `<button type='button' class='slick-arrow slick-prev pull-left'><i class="fa-solid fa-chevron-left"></i></button>`,
+  nextArrow:
+  `<button type='button' class='slick-arrow slick-next pull-right'><i class="fa-solid fa-chevron-right"></i></button>`,
+  asNavFor: '.photo-slide-bottom',
+  arrows:false,
+});
 
 });
 
