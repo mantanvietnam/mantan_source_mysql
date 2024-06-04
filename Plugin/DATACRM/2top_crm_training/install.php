1758a8c2fef0f467146f11e5907d7ec6614ad258
mantanvietnam <mantanvietnam@gmail.com>
2024-06-05 00:35:06 +0700

    update

--- Plugin/DATACRM/2top_crm_training/install.php
+++ Plugin/DATACRM/2top_crm_training/install.php
@@ -5,30 +5,79 @@ global $sqlUpdateDatabase;
 
 $sqlInstallDatabase = '';
 $sqlDeleteDatabase = '';
-$sqlUpdateDatabase = '';
+$sqlUpdateDatabase = [];
 
-$sqlInstallDatabase .= "CREATE TABLE `lessons` ( `id` INT NOT NULL AUTO_INCREMENT , `title` VARCHAR(255) CHARACTER SET utf8 COLLATE utf8_vietnamese_ci NOT NULL , `content` TEXT CHARACTER SET utf8 COLLATE utf8_vietnamese_ci NOT NULL , `id_course` INT NOT NULL , `image` VARCHAR(255) NOT NULL , `status` VARCHAR(255) NOT NULL , `description` VARCHAR(255) CHARACTER SET utf8 COLLATE utf8_vietnamese_ci NOT NULL , `slug` VARCHAR(255) NOT NULL , `view` INT NOT NULL DEFAULT '0' , `time_learn` INT NOT NULL , `author` VARCHAR(255) CHARACTER SET utf8 COLLATE utf8_vietnamese_ci NOT NULL , `youtube_code` VARCHAR(255) NOT NULL , PRIMARY KEY (`id`)) ENGINE = InnoDB;";
+$sqlInstallDatabase .= "CREATE TABLE `lessons` ( 
+	`id` INT NOT NULL AUTO_INCREMENT , 
+	`title` VARCHAR(255) CHARACTER SET utf8 COLLATE utf8_vietnamese_ci NOT NULL , 
+	`content` TEXT CHARACTER SET utf8 COLLATE utf8_vietnamese_ci NOT NULL , 
+	`id_course` INT NOT NULL , 
+	`image` VARCHAR(255) NOT NULL , 
+	`status` VARCHAR(255) NOT NULL , 
+	`description` VARCHAR(255) CHARACTER SET utf8 COLLATE utf8_vietnamese_ci NOT NULL , 
+	`slug` VARCHAR(255) NOT NULL , 
+	`view` INT NOT NULL DEFAULT '0' , 
+	`time_learn` INT NOT NULL , 
+	`author` VARCHAR(255) CHARACTER SET utf8 COLLATE utf8_vietnamese_ci NOT NULL , 
+	`youtube_code` VARCHAR(255) NOT NULL , 
+	PRIMARY KEY (`id`)
+) ENGINE = InnoDB;";
 
-$sqlInstallDatabase .= "CREATE TABLE `tests` ( `id` INT NOT NULL AUTO_INCREMENT , `description` TEXT CHARACTER SET utf8 COLLATE utf8_vietnamese_ci NOT NULL , `time_test` INT NOT NULL , `title` VARCHAR(255) CHARACTER SET utf8 COLLATE utf8_vietnamese_ci NOT NULL , `id_lesson` INT NOT NULL , `status` VARCHAR(255) NOT NULL , `slug` VARCHAR(255) NOT NULL , `time_start` INT NOT NULL , `time_end` INT NOT NULL , `id_course` INT NULL, `point_min` FLOAT NOT NULL DEFAULT '10', PRIMARY KEY (`id`)) ENGINE = InnoDB;";
+$sqlInstallDatabase .= "CREATE TABLE `tests` ( 
+	`id` INT NOT NULL AUTO_INCREMENT , 
+	`description` TEXT CHARACTER SET utf8 COLLATE utf8_vietnamese_ci NOT NULL , 
+	`time_test` INT NOT NULL , 
+	`title` VARCHAR(255) CHARACTER SET utf8 COLLATE utf8_vietnamese_ci NOT NULL , 
+	`id_lesson` INT NOT NULL , 
+	`status` VARCHAR(255) NOT NULL , 
+	`slug` VARCHAR(255) NOT NULL , 
+	`time_start` INT NOT NULL , 
+	`time_end` INT NOT NULL , 
+	`id_course` INT NULL, 
+	`point_min` FLOAT NOT NULL DEFAULT '10', 
+	PRIMARY KEY (`id`)
+) ENGINE = InnoDB;";
 
-$sqlInstallDatabase .= "CREATE TABLE `questions` ( `id` INT NOT NULL AUTO_INCREMENT , `question` TEXT CHARACTER SET utf8 COLLATE utf8_vietnamese_ci NOT NULL , `option_a` TEXT CHARACTER SET utf8 COLLATE utf8_vietnamese_ci NOT NULL , `option_b` TEXT CHARACTER SET utf8 COLLATE utf8_vietnamese_ci NOT NULL , `option_c` TEXT CHARACTER SET utf8 COLLATE utf8_vietnamese_ci NOT NULL , `option_d` TEXT CHARACTER SET utf8 COLLATE utf8_vietnamese_ci NOT NULL , `option_true` VARCHAR(255) NOT NULL , `id_test` INT NOT NULL , `status` VARCHAR(255) NOT NULL , PRIMARY KEY (`id`)) ENGINE = InnoDB;";
+$sqlInstallDatabase .= "CREATE TABLE `questions` ( 
+	`id` INT NOT NULL AUTO_INCREMENT , 
+	`question` TEXT CHARACTER SET utf8 COLLATE utf8_vietnamese_ci NOT NULL , 
+	`option_a` TEXT CHARACTER SET utf8 COLLATE utf8_vietnamese_ci NOT NULL , 
+	`option_b` TEXT CHARACTER SET utf8 COLLATE utf8_vietnamese_ci NOT NULL , 
+	`option_c` TEXT CHARACTER SET utf8 COLLATE utf8_vietnamese_ci NOT NULL , 
+	`option_d` TEXT CHARACTER SET utf8 COLLATE utf8_vietnamese_ci NOT NULL , 
+	`option_true` VARCHAR(255) NOT NULL , 
+	`id_test` INT NOT NULL , 
+	`status` VARCHAR(255) NOT NULL , 
+	PRIMARY KEY (`id`)
+) ENGINE = InnoDB;";
 
-$sqlInstallDatabase .= "CREATE TABLE `historytests` ( `id` INT NOT NULL AUTO_INCREMENT , `id_customer` INT NOT NULL , `id_test` INT NOT NULL , `point` FLOAT NOT NULL , `total_true` INT NOT NULL , `number_question` INT NOT NULL , `time_start` INT NOT NULL , `time_end` INT NOT NULL , `status` VARCHAR(255) NOT NULL, PRIMARY KEY (`id`)) ENGINE = InnoDB;";
+$sqlInstallDatabase .= "CREATE TABLE `historytests` ( 
+	`id` INT NOT NULL AUTO_INCREMENT , 
+	`id_customer` INT NOT NULL , 
+	`id_test` INT NOT NULL , 
+	`point` FLOAT NOT NULL , 
+	`total_true` INT NOT NULL , 
+	`number_question` INT NOT NULL , 
+	`time_start` INT NOT NULL , 
+	`time_end` INT NOT NULL , 
+	`status` VARCHAR(255) NOT NULL, 
+	PRIMARY KEY (`id`)
+) ENGINE = InnoDB;";
 
 $sqlInstallDatabase .= "CREATE TABLE `courses` ( 
-							`id` INT NOT NULL AUTO_INCREMENT , 
-							`title` VARCHAR(255) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL , 
-							`image` TEXT CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL , 
-							`description` VARCHAR(255) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL , 
-							`slug` VARCHAR(255) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL , 
-							`view` INT NOT NULL DEFAULT '0' , 
-							`youtube_code` VARCHAR(255) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL , 
-							`id_category` INT NOT NULL DEFAULT '0' , 
-							`status` VARCHAR(255) NOT NULL, 
-							`content` TEXT CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL, 
-							`public` BOOLEAN NOT NULL DEFAULT FALSE,
-							PRIMARY KEY (`id`)
-						) ENGINE = InnoDB; ";
+	`id` INT NOT NULL AUTO_INCREMENT , 
+	`title` VARCHAR(255) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL , 
+	`image` TEXT CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL , 
+	`description` VARCHAR(255) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL , 
+	`slug` VARCHAR(255) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL , 
+	`view` INT NOT NULL DEFAULT '0' , 
+	`youtube_code` VARCHAR(255) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL , 
+	`id_category` INT NOT NULL DEFAULT '0' , 
+	`status` VARCHAR(255) NOT NULL, 
+	`content` TEXT CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL, 
+	`public` BOOLEAN NOT NULL DEFAULT FALSE,
+	PRIMARY KEY (`id`)
+) ENGINE = InnoDB; ";
 
 
 $sqlDeleteDatabase .= "DROP TABLE lessons; ";
@@ -40,5 +89,60 @@ $sqlDeleteDatabase .= "DROP TABLE courses; ";
 $sqlDeleteDatabase .= "DELETE FROM `categories` WHERE `type`='2top_crm_training'; ";
 $sqlDeleteDatabase .= "DELETE FROM `options` WHERE `key_word`='settingTraining2TOPCRM'; ";
 
-$sqlUpdateDatabase .= "ALTER TABLE `courses` ADD `public` BOOLEAN NOT NULL DEFAULT FALSE AFTER `content`; ";
+// bảng lessons
+$sqlUpdateDatabase['lessons']['title'] = "ALTER TABLE `lessons` ADD `title` VARCHAR(255) CHARACTER SET utf8 COLLATE utf8_vietnamese_ci NOT NULL;";
+$sqlUpdateDatabase['lessons']['content'] = "ALTER TABLE `lessons` ADD `content` TEXT CHARACTER SET utf8 COLLATE utf8_vietnamese_ci NOT NULL;";
+$sqlUpdateDatabase['lessons']['id_course'] = "ALTER TABLE `lessons` ADD `id_course` INT NOT NULL;";
+$sqlUpdateDatabase['lessons']['image'] = "ALTER TABLE `lessons` ADD `image` VARCHAR(255) NOT NULL;";
+$sqlUpdateDatabase['lessons']['status'] = "ALTER TABLE `lessons` ADD `status` VARCHAR(255) NOT NULL;";
+$sqlUpdateDatabase['lessons']['description'] = "ALTER TABLE `lessons` ADD `description` VARCHAR(255) CHARACTER SET utf8 COLLATE utf8_vietnamese_ci NOT NULL;";
+$sqlUpdateDatabase['lessons']['slug'] = "ALTER TABLE `lessons` ADD `slug` VARCHAR(255) NOT NULL;";
+$sqlUpdateDatabase['lessons']['view'] = "ALTER TABLE `lessons` ADD `view` INT NOT NULL DEFAULT '0';";
+$sqlUpdateDatabase['lessons']['time_learn'] = "ALTER TABLE `lessons` ADD `time_learn` INT NOT NULL;";
+$sqlUpdateDatabase['lessons']['author'] = "ALTER TABLE `lessons` ADD `author` VARCHAR(255) CHARACTER SET utf8 COLLATE utf8_vietnamese_ci NOT NULL;";
+$sqlUpdateDatabase['lessons']['youtube_code'] = "ALTER TABLE `lessons` ADD `youtube_code` VARCHAR(255) NOT NULL;";
+
+// bảng tests
+$sqlUpdateDatabase['tests']['description'] = "ALTER TABLE `tests` ADD `description` TEXT CHARACTER SET utf8 COLLATE utf8_vietnamese_ci NOT NULL;";
+$sqlUpdateDatabase['tests']['time_test'] = "ALTER TABLE `tests` ADD `time_test` INT NOT NULL;";
+$sqlUpdateDatabase['tests']['title'] = "ALTER TABLE `tests` ADD `title` VARCHAR(255) CHARACTER SET utf8 COLLATE utf8_vietnamese_ci NOT NULL;";
+$sqlUpdateDatabase['tests']['id_lesson'] = "ALTER TABLE `tests` ADD `id_lesson` INT NOT NULL ;";
+$sqlUpdateDatabase['tests']['status'] = "ALTER TABLE `tests` ADD `status` VARCHAR(255) NOT NULL;";
+$sqlUpdateDatabase['tests']['slug'] = "ALTER TABLE `tests` ADD `slug` VARCHAR(255) NOT NULL;";
+$sqlUpdateDatabase['tests']['time_start'] = "ALTER TABLE `tests` ADD `time_start` INT NOT NULL;";
+$sqlUpdateDatabase['tests']['time_end'] = "ALTER TABLE `tests` ADD `time_end` INT NOT NULL;";
+$sqlUpdateDatabase['tests']['id_course'] = "ALTER TABLE `tests` ADD `id_course` INT NULL;";
+$sqlUpdateDatabase['tests']['point_min'] = "ALTER TABLE `tests` ADD `point_min` FLOAT NOT NULL DEFAULT '10';";
+
+// bảng questions
+$sqlUpdateDatabase['questions']['question'] = "ALTER TABLE `questions` ADD `question` TEXT CHARACTER SET utf8 COLLATE utf8_vietnamese_ci NOT NULL;";
+$sqlUpdateDatabase['questions']['option_a'] = "ALTER TABLE `questions` ADD `option_a` TEXT CHARACTER SET utf8 COLLATE utf8_vietnamese_ci NOT NULL;";
+$sqlUpdateDatabase['questions']['option_b'] = "ALTER TABLE `questions` ADD `option_b` TEXT CHARACTER SET utf8 COLLATE utf8_vietnamese_ci NOT NULL;";
+$sqlUpdateDatabase['questions']['option_c'] = "ALTER TABLE `questions` ADD `option_c` TEXT CHARACTER SET utf8 COLLATE utf8_vietnamese_ci NOT NULL;";
+$sqlUpdateDatabase['questions']['option_d'] = "ALTER TABLE `questions` ADD `option_d` TEXT CHARACTER SET utf8 COLLATE utf8_vietnamese_ci NOT NULL;";
+$sqlUpdateDatabase['questions']['option_true'] = "ALTER TABLE `questions` ADD `option_true` VARCHAR(255) NOT NULL;";
+$sqlUpdateDatabase['questions']['id_test'] = "ALTER TABLE `questions` ADD `id_test` INT NOT NULL;";
+$sqlUpdateDatabase['questions']['status'] = "ALTER TABLE `questions` ADD `status` VARCHAR(255) NOT NULL;";
+
+// bảng historytests
+$sqlUpdateDatabase['historytests']['id_customer'] = "ALTER TABLE `historytests` ADD `id_customer` INT NOT NULL;";
+$sqlUpdateDatabase['historytests']['id_test'] = "ALTER TABLE `historytests` ADD `id_test` INT NOT NULL;";
+$sqlUpdateDatabase['historytests']['point'] = "ALTER TABLE `historytests` ADD `point` FLOAT NOT NULL;";
+$sqlUpdateDatabase['historytests']['total_true'] = "ALTER TABLE `historytests` ADD `total_true` INT NOT NULL;";
+$sqlUpdateDatabase['historytests']['number_question'] = "ALTER TABLE `historytests` ADD `number_question` INT NOT NULL;";
+$sqlUpdateDatabase['historytests']['time_start'] = "ALTER TABLE `historytests` ADD `time_start` INT NOT NULL;";
+$sqlUpdateDatabase['historytests']['time_end'] = "ALTER TABLE `historytests` ADD `time_end` INT NOT NULL;";
+$sqlUpdateDatabase['historytests']['status'] = "ALTER TABLE `historytests` ADD `status` VARCHAR(255) NOT NULL;";
+
+// bảng courses
+$sqlUpdateDatabase['courses']['title'] = "ALTER TABLE `courses` ADD `title` VARCHAR(255) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL;";
+$sqlUpdateDatabase['courses']['image'] = "ALTER TABLE `courses` ADD `image` TEXT CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL;";
+$sqlUpdateDatabase['courses']['description'] = "ALTER TABLE `courses` ADD `description` VARCHAR(255) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL;";
+$sqlUpdateDatabase['courses']['slug'] = "ALTER TABLE `courses` ADD `slug` VARCHAR(255) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL;";
+$sqlUpdateDatabase['courses']['view'] = "ALTER TABLE `courses` ADD `view` INT NOT NULL DEFAULT '0';";
+$sqlUpdateDatabase['courses']['youtube_code'] = "ALTER TABLE `courses` ADD `youtube_code` VARCHAR(255) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL;";
+$sqlUpdateDatabase['courses']['id_category'] = "ALTER TABLE `courses` ADD `id_category` INT NOT NULL DEFAULT '0';";
+$sqlUpdateDatabase['courses']['status'] = "ALTER TABLE `courses` ADD `status` VARCHAR(255) NOT NULL;";
+$sqlUpdateDatabase['courses']['content'] = "ALTER TABLE `courses` ADD `content` TEXT CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL;";
+$sqlUpdateDatabase['courses']['public'] = "ALTER TABLE `courses` ADD `public` BOOLEAN NOT NULL DEFAULT FALSE;";
 ?>
--- Plugin/DATACRM/affiliate/install.php
+++ Plugin/DATACRM/affiliate/install.php
@@ -5,7 +5,7 @@ global $sqlUpdateDatabase;
 
 $sqlInstallDatabase = '';
 $sqlDeleteDatabase = '';
-$sqlUpdateDatabase = '';
+$sqlUpdateDatabase = [];
 
 $sqlInstallDatabase .= "CREATE TABLE `affiliaters` ( 
     `id` INT NOT NULL AUTO_INCREMENT , 
@@ -50,5 +50,37 @@ $sqlInstallDatabase .= "CREATE TABLE `transaction_affiliate_histories` (
 $sqlDeleteDatabase .= "DROP TABLE affiliaters; ";
 $sqlDeleteDatabase .= "DROP TABLE transaction_affiliate_histories; ";
 
-$sqlUpdateDatabase .= "ALTER TABLE `affiliaters` ADD `last_login` INT NOT NULL DEFAULT '0' AFTER `youtube`, ADD `portrait` VARCHAR(255) CHARACTER SET utf8mb3 COLLATE utf8mb3_unicode_ci NULL AFTER `last_login`; ";
+// bảng affiliaters
+$sqlUpdateDatabase['affiliaters']['name'] = "ALTER TABLE `affiliaters` ADD `name` VARCHAR(255) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL; ";
+$sqlUpdateDatabase['affiliaters']['phone'] = "ALTER TABLE `affiliaters` ADD `phone` VARCHAR(15) NOT NULL; ";
+$sqlUpdateDatabase['affiliaters']['email'] = "ALTER TABLE `affiliaters` ADD `email` VARCHAR(255) CHARACTER SET utf8 COLLATE utf8_unicode_ci NULL; ";
+$sqlUpdateDatabase['affiliaters']['address'] = "ALTER TABLE `affiliaters` ADD `address` VARCHAR(255) CHARACTER SET utf8 COLLATE utf8_unicode_ci NULL; ";
+$sqlUpdateDatabase['affiliaters']['id_father'] = "ALTER TABLE `affiliaters` ADD `id_father` INT NOT NULL DEFAULT '0' COMMENT 'id người giới thiệu'; ";
+$sqlUpdateDatabase['affiliaters']['id_customer'] = "ALTER TABLE `affiliaters` ADD `id_customer` INT NOT NULL DEFAULT '0'; ";
+$sqlUpdateDatabase['affiliaters']['id_member'] = "ALTER TABLE `affiliaters` ADD `id_member` INT NOT NULL DEFAULT '0'; ";
+$sqlUpdateDatabase['affiliaters']['avatar'] = "ALTER TABLE `affiliaters` ADD `avatar` VARCHAR(500) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL; ";
+$sqlUpdateDatabase['affiliaters']['id_system'] = "ALTER TABLE `affiliaters` ADD `id_system` INT NOT NULL DEFAULT '0'; ";
+$sqlUpdateDatabase['affiliaters']['password'] = "ALTER TABLE `affiliaters` ADD `password` VARCHAR(100) CHARACTER SET utf8 COLLATE utf8_unicode_ci NULL; ";
+$sqlUpdateDatabase['affiliaters']['created_at'] = "ALTER TABLE `affiliaters` ADD `created_at` INT NOT NULL; ";
+$sqlUpdateDatabase['affiliaters']['view'] = "ALTER TABLE `affiliaters` ADD `view` INT NOT NULL DEFAULT '0'; ";
+$sqlUpdateDatabase['affiliaters']['description'] = "ALTER TABLE `affiliaters` ADD `description` TEXT CHARACTER SET utf8 COLLATE utf8_unicode_ci NULL; ";
+$sqlUpdateDatabase['affiliaters']['linkedin'] = "ALTER TABLE `affiliaters` ADD `linkedin` VARCHAR(500) CHARACTER SET utf8 COLLATE utf8_unicode_ci NULL; ";
+$sqlUpdateDatabase['affiliaters']['web'] = "ALTER TABLE `affiliaters` ADD `web` VARCHAR(500) CHARACTER SET utf8 COLLATE utf8_unicode_ci NULL; ";
+$sqlUpdateDatabase['affiliaters']['instagram'] = "ALTER TABLE `affiliaters` ADD `instagram` VARCHAR(500) CHARACTER SET utf8 COLLATE utf8_unicode_ci NULL; ";
+$sqlUpdateDatabase['affiliaters']['zalo'] = "ALTER TABLE `affiliaters` ADD `zalo` VARCHAR(500) CHARACTER SET utf8 COLLATE utf8_unicode_ci NULL; ";
+$sqlUpdateDatabase['affiliaters']['facebook'] = "ALTER TABLE `affiliaters` ADD `facebook` VARCHAR(500) CHARACTER SET utf8 COLLATE utf8_unicode_ci NULL; ";
+$sqlUpdateDatabase['affiliaters']['twitter'] = "ALTER TABLE `affiliaters` ADD `twitter` VARCHAR(500) CHARACTER SET utf8 COLLATE utf8_unicode_ci NULL; ";
+$sqlUpdateDatabase['affiliaters']['tiktok'] = "ALTER TABLE `affiliaters` ADD `tiktok` VARCHAR(500) CHARACTER SET utf8 COLLATE utf8_unicode_ci NULL; ";
+$sqlUpdateDatabase['affiliaters']['youtube'] = "ALTER TABLE `affiliaters` ADD `youtube` VARCHAR(500) CHARACTER SET utf8 COLLATE utf8_unicode_ci NULL; ";
+$sqlUpdateDatabase['affiliaters']['last_login'] = "ALTER TABLE `affiliaters` ADD `last_login` INT NOT NULL DEFAULT '0'; ";
+$sqlUpdateDatabase['affiliaters']['portrait'] = "ALTER TABLE `affiliaters` ADD `portrait` VARCHAR(255) CHARACTER SET utf8mb3 COLLATE utf8mb3_unicode_ci NULL; ";
+
+// bảng transaction_affiliate_histories
+$sqlUpdateDatabase['transaction_affiliate_histories']['id_affiliater'] = "ALTER TABLE `transaction_affiliate_histories` ADD `id_affiliater` INT NOT NULL; ";
+$sqlUpdateDatabase['transaction_affiliate_histories']['money_total'] = "ALTER TABLE `transaction_affiliate_histories` ADD `money_total` INT NOT NULL; ";
+$sqlUpdateDatabase['transaction_affiliate_histories']['money_back'] = "ALTER TABLE `transaction_affiliate_histories` ADD `money_back` INT NOT NULL; ";
+$sqlUpdateDatabase['transaction_affiliate_histories']['percent'] = "ALTER TABLE `transaction_affiliate_histories` ADD `percent` FLOAT NOT NULL; ";
+$sqlUpdateDatabase['transaction_affiliate_histories']['id_order'] = "ALTER TABLE `transaction_affiliate_histories` ADD `id_order` INT NOT NULL; ";
+$sqlUpdateDatabase['transaction_affiliate_histories']['create_at'] = "ALTER TABLE `transaction_affiliate_histories` ADD `create_at` INT NOT NULL; ";
+$sqlUpdateDatabase['transaction_affiliate_histories']['status'] = "ALTER TABLE `transaction_affiliate_histories` ADD `status` VARCHAR(20) NOT NULL DEFAULT 'new'; ";
 ?>
