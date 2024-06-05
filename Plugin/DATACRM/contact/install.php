b0cafca2cc0f7aff02b2c62a579910e5d7b5fa1f
ductri0712 <ductri071297@gmail.com>
2024-06-05 12:51:17 +0700

    Update contact

--- Plugin/DATACRM/contact/install.php
+++ Plugin/DATACRM/contact/install.php
@@ -1,22 +1,32 @@
 <?php
 global $sqlInstallDatabase;
 global $sqlDeleteDatabase;
+global $sqlUpdateDatabase;
 
 $sqlInstallDatabase = '';
 $sqlDeleteDatabase = '';
+$sqlUpdateDatabase = [];
 
 $sqlInstallDatabase .= "
   CREATE TABLE IF NOT EXISTS contacts (
-    id            INT NOT NULL AUTO_INCREMENT,
-    name          VARCHAR(50) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
-    email         VARCHAR(50) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
-    phone_number  VARCHAR(50),
-    subject       TEXT CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
-    content       TEXT CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
-    submitted_at  INT NOT NULL,
+    `id` INT NOT NULL AUTO_INCREMENT,
+    `name` VARCHAR(50) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
+    `email` VARCHAR(50) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
+    `phone_number` VARCHAR(50),
+    `subject` TEXT CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
+    `content` TEXT CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
+    `submitted_at`INT NOT NULL,
     PRIMARY KEY (id)
   );
 ";
 
 $sqlDeleteDatabase .= "DROP TABLE contacts; ";
+
+// Bang contacts
+$sqlUpdateDatabase['contacts']['name'] = "ALTER TABLE `contacts` ADD `name` VARCHAR(50) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL; ";
+$sqlUpdateDatabase['contacts']['email'] = "ALTER TABLE `contacts` ADD `email` VARCHAR(50) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL; ";
+$sqlUpdateDatabase['contacts']['phone_number'] = "ALTER TABLE `contacts` ADD `phone_number` VARCHAR(50); ";
+$sqlUpdateDatabase['contacts']['subject'] = "ALTER TABLE `contacts` ADD `subject` TEXT CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL; ";
+$sqlUpdateDatabase['contacts']['content'] = "ALTER TABLE `contacts` ADD `content` TEXT CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL; ";
+$sqlUpdateDatabase['contacts']['submitted_at'] = "ALTER TABLE `contacts` ADD `submitted_at`INT NOT NULL; ";
 ?>
