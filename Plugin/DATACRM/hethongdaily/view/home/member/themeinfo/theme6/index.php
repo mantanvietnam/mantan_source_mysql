561065f1b937721eaba0e165a19df3975fab79d8
vugenzz <matlanhhontien23@gmail.com>
2024-07-25 00:47:30 +0700

    up

--- Plugin/DATACRM/hethongdaily/view/home/member/themeinfo/theme6/index.php
+++ Plugin/DATACRM/hethongdaily/view/home/member/themeinfo/theme6/index.php
@@ -277,27 +277,27 @@
                             </div>
 
                             <div id="info_customer">
-                                <div class=" p-4"> 
+                                <div class=" p-4 box-info"> 
                                    <div class="input-group input-group-sm mb-3">
                                       <label class="input-group-text" >Họ tên (*)</label>
-                                      <input type="text" class="form-control" aria-label="Sizing example input" id="full_name" name="full_name" value="" required />
+                                      <input type="text" class="form-control box" aria-label="Sizing example input" id="full_name" name="full_name" value="" required />
                                   </div>
                                   <div class="input-group input-group-sm mb-3">
                                       <label class="input-group-text" >Số điện thoại (*)</label>
-                                      <input type="text" class="form-control" aria-label="Sizing example input" id="phone" name="phone" value="" required />
+                                      <input type="text" class="form-control box" aria-label="Sizing example input" id="phone" name="phone" value="" required />
                                   </div>
                                   <div class="input-group input-group-sm mb-3">
                                       <label class="input-group-text" >Địa chỉ nhận hàng</label>
-                                      <input type="text" class="form-control" aria-label="Sizing example input" id="address" name="address" value="" />
+                                      <input type="text" class="form-control box" aria-label="Sizing example input" id="address" name="address" value="" />
                                   </div>
                                   <div class="input-group input-group-sm mb-3">
                                       <label class="input-group-text" >Ngày sinh</label>
                                       <!-- <input type="text" class="form-control datepicker" id="birthday" name="birthday" value="" /> -->
-                                      <input type="text" class="form-control datepicker"  name="birthday" id="birthday" />
+                                      <input type="text" class="form-control box datepicker"  name="birthday" id="birthday" />
                                   </div>
                                   <div class="input-group input-group-sm mb-3">
                                       <label class="input-group-text" >Mã giảm giá</label>
-                                      <input type="text" class="form-control" aria-label="Sizing example input" id="discountCode" onchange="searchDiscountCodeAgencyAPI()" name="discountCode" value="" />
+                                      <input type="text" class="form-control box" aria-label="Sizing example input" id="discountCode" onchange="searchDiscountCodeAgencyAPI()" name="discountCode" value="" />
                                       <label id="messdiscount"></label>
                                   </div>
                                 </div>
