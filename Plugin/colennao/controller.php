7f8e4f211c3ff88e9383bcf6cef59f45780e4338
GauBacCuc0210 <nguyenhoang02102003@gmail.com>
2024-09-06 14:03:22 +0700

    update

--- Plugin/colennao/controller.php
+++ Plugin/colennao/controller.php
@@ -6,6 +6,7 @@ include_once('controller/admin/coursesController.php');
 include_once('controller/admin/lessonsController.php'); 
 include_once('controller/admin/questionsController.php'); 
 include_once('controller/admin/testsController.php'); 
+include_once('controller/admin/fastingController.php'); 
 // Api .php
 include_once('controller/api/usersController.php'); // Tài xế
 include_once('controller/api/coursesController.php');
