5df25046b49a6b51e2976b94b16e09ce5d167924
huubang97 <69882867+huubang97@users.noreply.github.com>
2024-09-16 17:48:04 +0700

    Update challengeController.php

--- Plugin/colennao/controller/api/challengeController.php
+++ Plugin/colennao/controller/api/challengeController.php
@@ -9,6 +9,7 @@ function listChallengeAPI($input)
     $modelChallenge = $controller->loadModel('Challenges');
     $modelFeedbackChallenge = $controller->loadModel('FeedbackChallenges');
     $modelResultChallenges = $controller->loadModel('ResultChallenges');
+    $modelUserChallenges = $controller->loadModel('UserChallenges');
 
     if($isRequestPost){
     	$dataSend = $input['request']->getData();	
@@ -29,13 +30,24 @@ function listChallengeAPI($input)
 			        $conditions['title LIKE'] = '%' . $dataSend['title'] . '%';
 			    }
 
+			    $checkUserChallenges = $modelUserChallenges->find()->where(array('id_user'=> $user->id))->order(['id' => 'desc'])->all()->toList();
+			    $listid = array();
+
+			    if(!empty($checkUserChallenges)){
+			    	foreach($checkUserChallenges as $key => $item){
+			    		$listid[] = $item->id_challenge;
+			    	}
+			    }
+
+			    if(!empty($listid)){
+			    	$conditions['id NOT IN'] = $listid;
+			    }
 			    
-			    $listData = $modelChallenge->find()->limit($limit)->page($page)->where($conditions)->order(['id' => 'desc'])->all()->toList();
-			    
-			    
+			    $listData = $modelChallenge->find()->limit($limit)->page($page)->where($conditions)->order(['id' => 'desc'])->all()->toList();		   
+			   
 			    $totalData = count($modelChallenge->find()->where($conditions)->all()->toList());
 			        
-			    return apiResponse(0, 'lấy láy dữ liệu thành công', $listData, $totalData);
+			    return apiResponse(0, 'lấy dữ liệu thành công', $listData, $totalData);
 			}
 			 return apiResponse(3, 'Tài khoản không tồn tại hoặc chưa đăng nhập');
 		} 
@@ -185,10 +197,11 @@ function listUserChallengeAPI($input)
 
 			    
 			    $data = $modelUserChallenges->find()->where($conditions)->order(['id' => 'desc'])->all()->toList();
+
 			    $listData = array();
 			    if(!empty($data)){
 			    	foreach ($data as $key => $value) {
-		    			$Challenge = $modelChallenge->find()->where(array('id'=> $value->id))->first();
+		    			$Challenge = $modelChallenge->find()->where(array('id'=> $value->id_challenge))->first();
 		    			if(!empty($Challenge)){
 		    				$listData[]=  $Challenge;
 		    			}
@@ -199,7 +212,7 @@ function listUserChallengeAPI($input)
 			    
 			    // $totalData = count($modelChallenge->find()->where($conditions)->all()->toList());
 			        
-			    return apiResponse(0, 'lấy láy dữ liệu thành công', $listData);
+			    return apiResponse(0, 'lấy dữ liệu thành công', $listData);
 			}
 			 return apiResponse(3, 'Tài khoản không tồn tại hoặc chưa đăng nhập');
 		} 
@@ -208,6 +221,183 @@ function listUserChallengeAPI($input)
 	 return apiResponse(1, 'Bắt buộc sử dụng phương thức POST');
 }
 
+function getUserChallengeAPI($input)
+{
+    global $controller;
+    global $metaTitleMantan;
+    global $isRequestPost;
+
+    $metaTitleMantan = 'Danh sách thách thức';
+    $modelChallenge = $controller->loadModel('Challenges');
+    $modelFeedbackChallenge = $controller->loadModel('FeedbackChallenges');
+    $modelResultChallenges = $controller->loadModel('ResultChallenges');
+    $modelUserChallenges = $controller->loadModel('UserChallenges');
+    $modelcoach = $controller->loadModel('coach');
+
+    if($isRequestPost){
+    	$dataSend = $input['request']->getData();	
+    	 if(!empty($dataSend['token']) && !empty($dataSend['id_challenge'])){
+            $user = getUserByToken($dataSend['token']);
+            if(!empty($user)){
+			    $conditions = array('id_user'=> $user->id);
+			  
+			    $conditions['id_challenge'] = (int) $dataSend['id_challenge'];
+
+			    if (!empty($dataSend['title'])) {
+			        $conditions['title LIKE'] = '%' . $dataSend['title'] . '%';
+			    }
+
+			    
+			    $data = $modelUserChallenges->find()->where($conditions)->first();
+
+			    if(!empty($data)){
+			    	$Challenge = $modelChallenge->find()->where(array('id'=> $data->id_challenge))->first();
+			    	if(!empty($Challenge)){
+			    		
+				        $Challenge->feedback = $modelFeedbackChallenge->find()->where(['id_challenge'=>$Challenge->id])->all()->toList();
+				        $Challenge->result = $modelResultChallenges->find()->where(['id_challenge'=>$Challenge->id])->all()->toList();
+				        $Challenge->coach = $modelcoach->find()->where(['id'=>$Challenge->id_coach])->first();
+				    	$data->Challenge =  $Challenge;
+				    
+
+				    	$data->tip = json_decode($data->tip, true);
+				        
+				    	return apiResponse(0, 'lấy dữ liệu thành công', $data);
+					}
+					return apiResponse(4, 'Thử thách này không tồn tại');
+					
+			    }
+			    return apiResponse(5, 'bạn chưa mua thử thách này');
+		    	
+			}
+			 return apiResponse(3, 'Tài khoản không tồn tại hoặc chưa đăng nhập');
+		} 
+		return apiResponse(2, 'Gửi thiếu dữ liệu');  
+	}
+	 return apiResponse(1, 'Bắt buộc sử dụng phương thức POST');
+}
+
+function getTipUserChallengeAPI($input)
+{
+    global $controller;
+    global $metaTitleMantan;
+    global $isRequestPost;
+
+    $metaTitleMantan = 'Danh sách thách thức';
+    $modelChallenge = $controller->loadModel('Challenges');
+    $modelFeedbackChallenge = $controller->loadModel('FeedbackChallenges');
+    $modelResultChallenges = $controller->loadModel('ResultChallenges');
+    $modelUserChallenges = $controller->loadModel('UserChallenges');
+    $modelcoach = $controller->loadModel('coach');
+
+    if($isRequestPost){
+    	$dataSend = $input['request']->getData();	
+    	 if(!empty($dataSend['token']) && !empty($dataSend['id_challenge']) && !empty($dataSend['id_tip'])){
+            $user = getUserByToken($dataSend['token']);
+            if(!empty($user)){
+			    $conditions = array('id_user'=> $user->id);
+			  
+			    $conditions['id_challenge'] = (int) $dataSend['id_challenge'];
+
+			    if (!empty($dataSend['title'])) {
+			        $conditions['title LIKE'] = '%' . $dataSend['title'] . '%';
+			    }
+
+			    
+			    $data = $modelUserChallenges->find()->where($conditions)->first();
+
+			    if(!empty($data)){
+			    	$Challenge = $modelChallenge->find()->where(array('id'=> $data->id_challenge))->first();
+			    	if(!empty($Challenge)){
+			  			if(!empty($data->tip)){
+			  				$tip = json_decode($data->tip, true);
+			  				foreach($tip as $key => $item){
+			  					if($item['id']==$dataSend['id_tip']){
+			  						$data->tip = $item;
+			  					}
+			  				}
+			  			}
+				        
+				    	return apiResponse(0, 'lấy dữ liệu thành công', $data);
+					}
+					return apiResponse(4, 'Thử thách này không tồn tại');
+					
+			    }
+			    return apiResponse(4, 'bạn chưa mua thử thách này');
+		    	
+			}
+			 return apiResponse(3, 'Tài khoản không tồn tại hoặc chưa đăng nhập');
+		} 
+		return apiResponse(2, 'Gửi thiếu dữ liệu');  
+	}
+	 return apiResponse(1, 'Bắt buộc sử dụng phương thức POST');
+}
+
+function updateStatusTipUserChallengeAPI($input)
+{
+    global $controller;
+    global $metaTitleMantan;
+    global $isRequestPost;
+
+    $metaTitleMantan = 'Danh sách thách thức';
+    $modelChallenge = $controller->loadModel('Challenges');
+    $modelFeedbackChallenge = $controller->loadModel('FeedbackChallenges');
+    $modelResultChallenges = $controller->loadModel('ResultChallenges');
+    $modelUserChallenges = $controller->loadModel('UserChallenges');
+    $modelcoach = $controller->loadModel('coach');
+
+    if($isRequestPost){
+    	$dataSend = $input['request']->getData();	
+    	 if(!empty($dataSend['token']) && !empty($dataSend['id_challenge']) && !empty($dataSend['id_tip']) && !empty($dataSend['status'])){
+            $user = getUserByToken($dataSend['token']);
+            if(!empty($user)){
+			    $conditions = array('id_user'=> $user->id);
+			  
+			    $conditions['id_challenge'] = (int) $dataSend['id_challenge'];
+
+			    if (!empty($dataSend['title'])) {
+			        $conditions['title LIKE'] = '%' . $dataSend['title'] . '%';
+			    }
+
+			    
+			    $data = $modelUserChallenges->find()->where($conditions)->first();
+			    $tips = array();
+			    if(!empty($data)){
+			    	$Challenge = $modelChallenge->find()->where(array('id'=> $data->id_challenge))->first();
+			    	if(!empty($Challenge)){
+			  			if(!empty($data->tip)){
+
+			  				$tip = json_decode($data->tip, true);
+			  				foreach($tip as $key => $item){
+			  					if($item['id']==$dataSend['id_tip']){
+			  						$item['status'] = $dataSend['status'];
+
+			  						$tip[$key] =  $item;
+
+			  						$tips = $item;
+			  					}
+			  				}
+			  				$data->tip = json_encode($tip);
+
+			  			}
+
+			  			$modelUserChallenges->save($data);
+			  			$data->tip = $tips; 
+				        
+				    	return apiResponse(0, 'cập nhập dữ liệu thành công', $data);
+					}
+					return apiResponse(4, 'Thử thách này không tồn tại');
+					
+			    }
+			    return apiResponse(4, 'bạn chưa mua thử thách này');
+		    	
+			}
+			 return apiResponse(3, 'Tài khoản không tồn tại hoặc chưa đăng nhập');
+		} 
+		return apiResponse(2, 'Gửi thiếu dữ liệu');  
+	}
+	 return apiResponse(1, 'Bắt buộc sử dụng phương thức POST');
+}
 function checkprocessAddMoney(){
 
 	$mess = processAddMoney(500000, 1);
