9db910ceba324db3676097d179715823b11acf4e
GauBacCuc0210 <nguyenhoang02102003@gmail.com>
2024-09-21 08:46:27 +0700

    Update questionsController.php

--- Plugin/colennao/controller/api/questionsController.php
+++ Plugin/colennao/controller/api/questionsController.php
@@ -1,20 +1,4 @@
 <?php 
-function listquestionAPI($input) {
-    global $controller;
-    global $isRequestPost;
-    global $modelOptions;
-    global $modelCategories;
-    global $urlCurrent;
-    global $metaTitleMantan;
-    $metaTitleMantan = 'danh sách câu hỏi';
 
-    $modelquestions = $controller->loadModel('questions');
-    $Modelanswerquestion = $controller->loadModel('answerquestion');
-    if($isRequestPost){
-        $dataSend = $input['request']->getData();
-        
-    }
-
-}
 
 ?>
