179e4b787f27eed7c2ea261e3d67e9da4120fbc4
GauBacCuc0210 <nguyenhoang02102003@gmail.com>
2024-09-21 08:49:23 +0700

    update

--- Plugin/colennao/controller/api/questionsController.php
+++ Plugin/colennao/controller/api/questionsController.php
@@ -1,4 +1,36 @@
 <?php 
+function listquestionAPI($input)
+{
+    global $controller;
+    global $isRequestPost;
+    global $modelOptions;
+    global $modelCategories;
+    global $urlCurrent;
+    global $metaTitleMantan;
 
+    $metaTitleMantan = 'Danh sách câu hỏi';
+    $modelQuestions = $controller->loadModel('Questions');
+    $modelTests = $controller->loadModel('Tests'); 
+
+    if ($isRequestPost) {
+        $dataSend = $input['request']->getData();
+        $conditions = array();
+        $page = (!empty($dataSend['page'])) ? (int)$dataSend['page'] : 1;
+        $limit = (!empty($dataSend['limit'])) ? (int)$dataSend['limit'] : 20;
+        if ($page < 1) $page = 1;
+        $order = array('id' => 'desc');
+        if (!empty($dataSend['type'])) {
+            $name = $dataSend['type'];
+            $conditions['type LIKE'] = '%'. $name.'%';
+        }
+        $listData = $modelQuestions->find()->limit($limit)->page($page)->where($conditions)->order($order)->all()->toList();
+        $totalData = $modelQuestions->find()->where($conditions)->count(); 
+        $return = array('code' => 1, 'mess' => 'Lấy dữ liệu thành công', 'listData' => $listData, 'totalData' => $totalData);
+    } else {
+        $return = array('code' => 0, 'mess' => 'Gửi sai kiểu POST');
+    }
+
+    return $return;
+}
 
 ?>
