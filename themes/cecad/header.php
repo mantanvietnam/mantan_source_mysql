8be7247ef91fdb7b00b7b3c14750f595a4dc8727
lvh-8102 <lehieu8102002@gmail.com>
2024-10-28 09:08:52 +0700

    cecad

--- themes/cecad/header.php
+++ themes/cecad/header.php
@@ -7,7 +7,7 @@
     <title>CECAD</title>
     <link rel="stylesheet" href="<?= $urlThemeActive?>/asset/css/footer.css?time=00000">
     <link rel="stylesheet" href="<?= $urlThemeActive?>/asset/css/header.css?time=326047">
-    <link rel="stylesheet" href="<?= $urlThemeActive?>/asset/css/main-plus.css?time=326161018">
+    <link rel="stylesheet" href="<?= $urlThemeActive?>/asset/css/main-plus.css?time=326161019">
     <link rel="stylesheet" href="<?= $urlThemeActive?>/asset/css/main-hoang.css?time=654006">
     <!-- Slick Carousel CSS -->
     <link rel="stylesheet" type="text/css" href="https://cdn.jsdelivr.net/npm/slick-carousel@1.8.1/slick/slick.css" />
