164c729b9fe1b8fa89492eb4f4712f9c54decc10
vugenzz <matlanhhontien23@gmail.com>
2024-11-09 14:40:26 +0700

    up

---
+++ Plugin/DATACRM/hethongdaily/view/home/member/themeinfo/theme7/routes.php
@@ -0,0 +1,17 @@
+<?php
+   global $routesTheme;
+
+/*   $routesTheme['news'] = 'shopbanhang/news.php';
+   $routesTheme['chinh-sach-bao-hanh'] = 'shopbanhang/guarantee.php';
+   $routesTheme['huong-dan-kich-hoat-bao-hanh'] = 'shopbanhang/instruction.php';
+   $routesTheme['review'] = 'shopbanhang/review.php';
+   $routesTheme['nhan-xet-tu-kol'] = 'shopbanhang/reviewkol.php';
+   $routesTheme['khach-hang-dap-hop'] = 'shopbanhang/reviewBeatbox.php';
+   $routesTheme['review-san-pham'] = 'shopbanhang/reviewProduct.php';
+   $routesTheme['aboutus'] = 'shopbanhang/aboutus.php';*/
+
+   // việt hóa
+   $routesTheme['thu-vien-anh'] = 'Kyiova/detailAlbum.php';
+   $routesTheme['cau-chuyen-kyiova'] = 'Kyiova/aboutUs.php';
+   // $routesTheme['tin-tuc'] = 'shopbanhang/news.php';
+?>
