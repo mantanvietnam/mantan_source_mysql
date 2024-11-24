8e89525d0088eb0216aece9915d417c7710e8dfc
Trần Mạnh <mantanvietnam@gmail.com>
2024-11-24 22:55:57 +0700

    update

--- themes/truyenthongao/header.php
+++ themes/truyenthongao/header.php
@@ -7,7 +7,7 @@
     <?php mantan_header();
      global $settingThemes;
      ?>
-    <link rel="stylesheet" href="<?php echo $urlThemeActive;?>/asset/css/toptop.css?time=1215">
+    <link rel="stylesheet" href="<?php echo $urlThemeActive;?>/asset/css/toptop.css?time=122qs15">
     <!-- boostrap -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
     <!-- icon -->
