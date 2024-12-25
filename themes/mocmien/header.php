fbe93e27412bf4e1d87113d26f4282a017221471
ductri0712 <ductri071297@gmail.com>
2024-12-25 17:24:39 +0700

    update header.php

--- themes/mocmien/header.php
+++ themes/mocmien/header.php
@@ -10,8 +10,8 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Mộc Miên</title>
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
-  <link rel="stylesheet" href="<?= $urlThemeActive?>/styles/globle.css">
-  <link rel="stylesheet" href="<?= $urlThemeActive?>/styles/index.css">
+  <link rel="stylesheet" href="<?php echo @$urlThemeActive; ?>styles/globle.css">
+  <link rel="stylesheet" href="<?php echo @$urlThemeActive; ?>styles/index.css">
   <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css" rel="stylesheet">
   <link rel="stylesheet" type="text/css" href="starability-minified/starability-all.min.css"/>
   <link
