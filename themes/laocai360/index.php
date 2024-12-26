ef3750455a0445bc22bc54e8bc455b0783a42f8a
huubang97 <69882867+huubang97@users.noreply.github.com>
2024-12-26 23:19:16 +0700

    Update index.php

--- themes/laocai360/index.php
+++ themes/laocai360/index.php
@@ -26,7 +26,7 @@ global $urlThemeActive;
   
               <!-- Nội dung hero overlay -->
               <div class="hero-overlay" id="heroOverlay">
-                  <a href="<?php echo $setting['link_image360'] ?>" target="_blank" class="btn btn-primary" id="view360Btn">Xem toàn cảnh 360</a>
+                  <a href="<?php echo $setting['link_image360'] ?>"  class="btn btn-primary" id="view360Btn">Xem toàn cảnh 360</a>
               </div>
           </div>
       </div>
