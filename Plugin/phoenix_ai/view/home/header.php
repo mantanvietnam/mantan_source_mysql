813de6e327b55943fc3b594f70c2e50350cb92cb
GauBacCuc0210 <149450614+GauBacCuc0210@users.noreply.github.com>
2025-01-23 13:42:02 +0700

    update

--- Plugin/phoenix_ai/view/home/header.php
+++ Plugin/phoenix_ai/view/home/header.php
@@ -12,7 +12,7 @@
     <link rel="stylesheet" href="/plugins/phoenix_ai/view/home/assets/css/all.min.css">
     <link rel="stylesheet" href="/plugins/phoenix_ai/view/home/assets/css/style.css?index=921">
     <link rel="stylesheet" href="/plugins/phoenix_ai/view/home/assets/css/mainvu.css?index=2711116">
-    <link rel="stylesheet" href="/plugins/phoenix_ai/view/home/assets/css/mainhoang.css?index=1221132">
+    <link rel="stylesheet" href="/plugins/phoenix_ai/view/home/assets/css/mainhoang.css?index=1221133">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.1/css/all.min.css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.0/css/all.min.css" integrity="sha512-9xKTRVabjVeZmc+GUW8GgSmcREDunMM+Dt/GrzchfN8tkwHizc5RP4Ok/MXFFy5rIjJjzhndFScTceq5e6GvVQ==" crossorigin="anonymous" referrerpolicy="no-referrer" />
