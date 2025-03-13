6f2220abb0764d394d0b9f5f74d00f669e379a45
vugenzzz <matlanhhontien23@gmail.com>
2025-03-13 19:52:26 +0700

    up

--- themes/vinhomes/project/compare.php
+++ themes/vinhomes/project/compare.php
@@ -332,6 +332,7 @@ tr:hover td {
 </div>
 <?php getFooter(); ?>
 
+<!-- Bootstrap JavaScript -->
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
 <script>
     document.addEventListener("DOMContentLoaded", function() {
